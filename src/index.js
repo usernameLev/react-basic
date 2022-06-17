1a51db2017a5c31e0a87911b92d04b7008847042
Lev <tyanlev1989@gmail.com>
2022-06-17 22:51:11 +0500

    write any elements and components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
-const text = 'hello how are you?';
-
-const elem = (
-  <div>
-    <h2 className=''>{text}</h2>
-    <label htmlFor=''></label>
-    <button tabIndex='0'>Click</button>
-  </div>
-);
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(elem);
+root.render(<App />);
