1c45494f4c77fabbae5cc5ed62302bb0085502f6
Lev <tyanlev1989@gmail.com>
2022-06-18 16:42:14 +0500

    Working with forms, managed and unmanaged components

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,50 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-export const EmployeesAddForm = () => (
-  <div className='app-add-form'>
-    <h3>Добавьте нового сотрудника</h3>
-    <form className='add-form d-flex'>
-      <input
-        type='text'
-        className='form-control new-post-label'
-        placeholder='Как его зовут?'
-      />
-      <input
-        type='number'
-        className='form-control new-post-label'
-        placeholder='З/П в $?'
-      />
+export class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
 
-      <button type='submit' className='btn btn-outline-light'>
-        Добавить
-      </button>
-    </form>
-  </div>
-);
+  onValueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className='app-add-form'>
+        <h3>Добавьте нового сотрудника</h3>
+        <form className='add-form d-flex'>
+          <input
+            type='text'
+            className='form-control new-post-label'
+            placeholder='Как его зовут?'
+            onChange={this.onValueChange}
+            name='name'
+            value={name}
+          />
+          <input
+            type='number'
+            className='form-control new-post-label'
+            placeholder='З/П в $?'
+            onChange={this.onValueChange}
+            name='salary'
+            value={salary}
+          />
+
+          <button type='submit' className='btn btn-outline-light'>
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
