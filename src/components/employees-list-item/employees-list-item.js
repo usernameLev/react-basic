d68395ed06c309111b9ea668f3fe1e64d892890e
Lev <tyanlev1989@gmail.com>
2022-06-18 22:25:20 +0500

    Test own events

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -3,28 +3,27 @@ import './employees-list-item.css';
 export const EmployeesListItem = ({ name, salary, increase }) => {
   let classNames = 'list-group-item d-flex justify-content-between';
   if (increase) {
-    classNames += ' increase'
+    classNames += ' increase';
   }
 
   return (
     <li className={classNames}>
-    <span className='list-group-item-label'>{name}</span>
-    <input
-      className='list-group-item-input'
-      type='text'
-      defaultValue={`${salary} $`}
-    />
-    <div className='d-flex justify-content-center align-items-center'>
-      <button className='btn-cookie btn-sm' type='button'>
-        <i className='fas fa-cookie'></i>
-      </button>
+      <span className='list-group-item-label'>{name}</span>
+      <input
+        className='list-group-item-input'
+        type='text'
+        defaultValue={`${salary} $`}
+      />
+      <div className='d-flex justify-content-center align-items-center'>
+        <button className='btn-cookie btn-sm' type='button'>
+          <i className='fas fa-cookie'></i>
+        </button>
 
-      <button className='btn-trash btn-sm' type='button'>
-        <i className='fas fa-trash'></i>
-      </button>
-      <i className='fas fa-star'></i>
-    </div>
-  </li>
-)
-
-  }
+        <button className='btn-trash btn-sm' type='button'>
+          <i className='fas fa-trash'></i>
+        </button>
+        <i className='fas fa-star'></i>
+      </div>
+    </li>
+  );
+};
